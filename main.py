1c042e3715c53f9cebd93bb7cbeaf898bd3894e5
Dushyant kumar <dushyantkr8810@gmail.com>
2026-04-18 04:45:54 -0700

    adding option of create a folder

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ print("1. Create a folder.")
 print("2. Read files and folders.")
 print("3. Update the folder.")
 print("4. Delte te folder.")
+print("5. creating a file.")
+print("6. update the file.")
+print("7. Delete the file.")
+
 
 option = int(input("Choose an option : "))
 
@@ -33,7 +37,7 @@ def update_folder():
         read_folder()                                                           # providing the files and folders name
         name = input("Enter your folder name that you want to update : ")       # taking folder name input from user for rename the preexisting file
         p = Path(name)                                                          # store path of it in a variable 
-        if p.exists() and p.is_dir():                                           # checking the condition tat the inputed folder name is exist or not
+        if p.exists() and p.is_dir():                                           # checking the condition that the inputed folder name is exist or not
             new_name = input("Enter new name of the folder : ")                 # taking folder new name as input from user
             p.rename(new_name)                                                  # changing the folder/file name
             print(f"Your folder succesfully update to {new_name}")              
@@ -44,16 +48,39 @@ def update_folder():
 
 def delete_folder():
     try:
-        read_folder()
-        name = input("Enter the name of folder that you want to delete : ")
-        p = Path(name)
-        if p.exists() and p.is_dir():
-            p.rmdir()
+        read_folder()                                                           # providing the files and folders name
+        name = input("Enter the name of folder that you want to delete : ")     # taking folder name input from user for rename the preexisting file
+        p = Path(name)                                                          # store path of it in a variable
+        if p.exists() and p.is_dir():                                           # checking the condition that the inputed folder name is exist or not
+            p.rmdir()                                                           # rmdir() is used for deleting the path  
             print("Your file succesfully deleted.")
         else:
             print("your file doesnot exist!!")
     except Exception as err:
         print(f"Showing error as {err}")
+
+
+def create_file():
+    try:
+        read_folder()                                                           # providing the files and folders name
+        name = input("Enter the file name that you want to create : ")          # taking folder name input from user for rename the preexisting file
+        p = Path(name)                                                          # store path of it in a variable
+        if not p.exists() :                                                     # checking the condition that if file is exist or not then making the existance of it
+            with open(name,'w') as file:                                        # creating a file
+                print("1. for write data in file\n2. for not write a data ")    # providing options like that you want to print some data in you file as at time of creation
+                opt = int(input("Enter option : "))                             # Taking option from user
+                if opt == 1 :                                                   # checking the condition as per user 
+                    data = input("Enter data that you want in file : ")
+                if opt ==2:
+                    pass
+                file.write(data)                                                #adding a written data in file
+            print("Your file is succesfully created.")
+        else:
+            print("Your file is already exist.")
+    except Exception as err:
+        print(f"Error as {err}")
+
+
 
 
 
@@ -68,6 +95,10 @@ if option==3:
 
 if option ==4:
     delete_folder()
+
+if option==5:
+    create_file()
+
 
 
 
